1e5cade98dd386b3e8481c5b3c003e7f113028e6
FarihulR <farihulrouf@gmail.com>
2021-07-04 08:21:19 +0700

    add Breadcrump

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import LandingPage from 'pages/LandingPages';
+import { BrowserRouter as Router, Route } from "react-router-dom"; 
 
+/*import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"; */
+
+/*import { Router, Route, Switch } from "react-router-dom"; */
+import LandingPage from 'pages/LandingPages';
+import Example from "pages/Example";
+import NotFound from 'pages/404'
+import BreadCrumb from 'pages/BreadCrumb'
 import "assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/example" component={Example} />
+        <Route exact path="/breadcrumb" component={BreadCrumb} />
+        <Route exact path="/404" component={NotFound}/>
       </Router>
     </div>
   );
